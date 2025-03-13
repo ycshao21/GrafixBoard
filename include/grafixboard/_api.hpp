b8904b19a1672d4b58ef828628e018000792ca68
jamesnulliu <jamesnulliu@gmail.com>
2025-03-13 08:21:59 +0000

    [FIX] Fix GRAPHIXCORE API

--- include/grafixboard/_api.hpp
+++ include/grafixboard/_api.hpp
@@ -1,18 +1,18 @@
 #pragma once
 
 #if defined(_WIN32) || defined(__WIN32__)
-    #if defined(GRAFIXBOARD_EXPORT)
-        #define GRAFIXBOARD_API __declspec(dllexport)
-    #elif defined(GRAFIXBOARD_IMPORT)
-        #define GRAFIXBOARD_API __declspec(dllimport)
+    #if defined(GRAFIXCORE_EXPORT)
+        #define GRAFIXCORE_API __declspec(dllexport)
+    #elif defined(GRAFIXCORE_IMPORT)
+        #define GRAFIXCORE_API __declspec(dllimport)
     #else
-        #define GRAFIXBOARD_API
+        #define GRAFIXCORE_API
     #endif
 #else
-    #if defined(GRAFIXBOARD_EXPORT)
-        #define GRAFIXBOARD_API                                    \
+    #if defined(GRAFIXCORE_EXPORT)
+        #define GRAFIXCORE_API                                    \
             __attribute__((visibility("default")))
     #else
-        #define GRAFIXBOARD_API
+        #define GRAFIXCORE_API
     #endif
 #endif
